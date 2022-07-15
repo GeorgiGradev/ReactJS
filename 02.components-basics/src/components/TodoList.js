41b16673c07be7903e58f065c325b6110fab92b2
Georgi Gradev <georgi.d.gradev@gmail.com>
2022-07-15 20:08:00 +0300

    Remove logos

---
+++ 02.components-basics/src/components/TodoList.js
@@ -0,0 +1,13 @@
+export default function TodoList() {
+    return (
+        <>
+            <h2>Tasks</h2>
+
+            <ul>
+                <li>Clean your room</li>
+                <li>Go shopping</li>
+                <li>Learn React</li>
+            </ul>
+        </>
+    );
+};
