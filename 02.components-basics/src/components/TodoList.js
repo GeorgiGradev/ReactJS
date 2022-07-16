143a78853676fb8bb61d610925593fdd4e1076b7
Georgi Gradev <georgi.d.gradev@gmail.com>
2022-07-16 12:33:43 +0300

    Add props

--- 02.components-basics/src/components/TodoList.js
+++ 02.components-basics/src/components/TodoList.js
@@ -1,12 +1,22 @@
+import TodoListItem from "./TodoListItem";
+
 export default function TodoList() {
     return (
         <>
             <h2>Tasks</h2>
 
+            {/* <ul>
+                <TodoListItem text="Clean your room" />
+                <TodoListItem text="Go shopping" />
+                <TodoListItem text="Learn React" />
+                <TodoListItem text="Learn class components" />
+            </ul> */}
+
             <ul>
-                <li>Clean your room</li>
-                <li>Go shopping</li>
-                <li>Learn React</li>
+                <TodoListItem>Clean your room</TodoListItem>
+                <TodoListItem>Go shopping</TodoListItem>
+                <TodoListItem>Learn React</TodoListItem>
+                <TodoListItem>Learn class components</TodoListItem>
             </ul>
         </>
     );
---
+++ 02.components-basics/src/components/TodoListItem.js
@@ -0,0 +1,5 @@
+const TodoListItem = (props) => {
+    // return <li>{props.text}</li>
+    return <li>{props.children}</li>
+}
+export default TodoListItem;
