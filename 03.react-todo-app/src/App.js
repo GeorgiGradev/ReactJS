1de69a441d0a5b979b57c4a489053db79aec3871
Georgi Gradev <georgi.d.gradev@gmail.com>
2022-07-18 12:26:25 +0300

    Add new features

---
+++ 03.react-todo-app/src/App.js
@@ -0,0 +1,12 @@
+import TodoList from './components/todoList';
+
+function App() {
+
+    return (
+        <div className="site-wrapper">
+            <TodoList />
+        </div>
+    );
+}
+
+export default App;
